e5396fc04c12640aaf829105c26c85967d5dddc3
RalfiSlask <nilssonnarki@gmail.com>
2024-05-20 09:06:53 +0200

    🔧 add colors for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBG: '#07001A',
+        secondaryBG: '#130D25',
+        primaryButton: '#B91B5D',
+        primaryButtonHover: '#A84970',
+        selectorBG: '#2A253A',
+        selectorBGHover: '#4c4368',
+        selectorActive: '#1BB96B',
+        selectorActiveHover: '#1BB96B',
+        special: '#B98D1B',
+      },
+    },
   },
   plugins: [],
-}
-
+};
