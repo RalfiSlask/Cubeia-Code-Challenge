2280bb565747cc116cf5e42cbb77ed7d4a6a4602
RalfiSlask <nilssonnarki@gmail.com>
2024-05-20 12:09:32 +0200

    ✨ add pick studios component and funcitonality

---
+++ src/components/main/PickStudios.tsx
@@ -0,0 +1,33 @@
+import StudiosModal from './StudiosModal';
+import { useContext } from 'react';
+import { Context } from '../../context/Context';
+import arrowLogo from '../../assets/icons/filter.svg';
+
+const PickStudios = () => {
+  const context = useContext(Context);
+
+  if (!context) return;
+
+  const { modalStates, toggleModalOnClick } = context;
+
+  return (
+    <>
+      <div className="bg-secondaryBG flex items-center w-[300px] justify-between py-4 px-4">
+        <p className="uppercase text-gray-400">Games By: </p>
+        <button
+          onClick={() => {
+            toggleModalOnClick('studios');
+          }}
+          className="flex items-center gap-2 w-[100px]"
+        >
+          <p className="flex w-[150px] flex-wrap gap-4 text-xl">Studios</p>
+          <img src={arrowLogo} alt="filter icon" width="24" height="24" />
+        </button>
+      </div>
+
+      {modalStates.studios && <StudiosModal />}
+    </>
+  );
+};
+
+export default PickStudios;
