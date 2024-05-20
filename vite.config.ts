0147b9275792f141268a7a64b2e1663fdada00d6
RalfiSlask <nilssonnarki@gmail.com>
2024-05-20 15:42:09 +0200

    🚀 Add base for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Cubeia-Code-Challenge/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
