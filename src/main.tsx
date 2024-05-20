24d097a99be61bbbcbdc80b9c4920f007dbe104d
RalfiSlask <nilssonnarki@gmail.com>
2024-05-20 10:26:24 +0200

    ✨ provide context and import styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './styles/styles.scss';
+import { ContextProvider } from './context/Context.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <ContextProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ContextProvider>
+);
