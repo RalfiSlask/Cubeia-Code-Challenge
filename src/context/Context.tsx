964f96ed5b7f67c5f99006784b1a1b873bd7bcd2
RalfiSlask <nilssonnarki@gmail.com>
2024-05-20 14:12:41 +0200

    ✨ Functionality for filtering games, setting categories, sorting studios, load more buttons

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useState, useEffect } from 'react';
-import { IModalStates, ICategory, IStudio } from '../utils/types/types';
+import { createContext, ReactNode, useState, useEffect, useMemo } from 'react';
+import { IModalStates, ICategory, IStudio, IGame } from '../utils/types/types';
 import data from '../data.json';
 import axios from 'axios';
 
@@ -7,15 +7,16 @@ interface IContext {
   activeCategory: number | null;
   modalStates: IModalStates;
   selectedCurrency: string;
+  numberOfGamesVisible: number;
+  filteredGames: IGame[];
   categories: ICategory[];
   studios: IStudio[];
-  showAllGames: boolean;
   stagedSelectedStudios: number[];
-  toggleShowAllGamesOnClick: (state: boolean) => void;
   toggleModalOnClick: (modalKey: keyof IModalStates) => void;
   handleClickOnCurrency: (currency: string) => void;
   handleClickOnSelectStudio: (studioId: number) => void;
   handleClickOnCategory: (categoryId: number | null) => void;
+  handleClickOnLoadMoreGames: (load: boolean) => void;
   handleClickOnSelectButton: () => void;
   handleClickOnResetStudios: () => void;
 }
@@ -31,10 +32,10 @@ export const ContextProvider: React.FC<IChildren> = ({ children }) => {
   const [modalStates, setModalStates] = useState<IModalStates>({ currencies: false, studios: false });
   const [selectedCurrency, setSelectedCurrency] = useState('EUR');
   const [categories, setCategories] = useState<ICategory[]>([]);
-  const [showAllGames, setShowAllGames] = useState(false);
   const [activeCategory, setActiveCategory] = useState<number | null>(null);
   const [stagedSelectedStudios, setStagedSelectedStudios] = useState<number[]>([]);
   const [selectedStudios, setSelectedStudios] = useState<number[]>([]);
+  const [numberOfGamesVisible, setNumberOfGamesVisible] = useState(50);
 
   /*   useEffect(() => {
     const fetchCasinoData = async () => {
@@ -51,12 +52,43 @@ export const ContextProvider: React.FC<IChildren> = ({ children }) => {
     fetchCasinoData();
   }, []); */
 
+  useEffect(() => {
+    console.log('studios', data.studios);
+    data.studios.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+    setStudios(data.studios);
+  }, []);
+
+  useEffect(() => {
+    // Add All category to categories array
+    const categoryAll: ICategory = { id: null, name: 'All', display: true, nameId: '', translations: {} };
+    const filteredCategories = data.tags.filter(tag => {
+      return data.games.some(game => game.gameTags.includes(tag.id));
+    });
+    setCategories([categoryAll, ...filteredCategories]);
+  }, []);
+
+  const filteredGames = useMemo(() => {
+    return data.games.filter(game => {
+      const categorySelected = activeCategory === null || game.gameTags.includes(activeCategory);
+      const selectedStudiosSelected = selectedStudios.length === 0 || selectedStudios.includes(game.studioId);
+
+      const studio = studios.find(studio => studio.id === game.studioId);
+      const currencyIsAllowed = studio ? !studio?.blockedCurrencies?.split(',').includes(selectedCurrency) : true;
+
+      return categorySelected && selectedStudiosSelected && currencyIsAllowed;
+    });
+  }, [activeCategory, selectedStudios, selectedCurrency, studios]);
+
   const toggleModalOnClick = (modalKey: keyof IModalStates) => {
     setModalStates(prev => ({ ...prev, [modalKey]: !prev[modalKey] }));
-  };
-
-  const toggleShowAllGamesOnClick = (state: boolean) => {
-    setShowAllGames(state);
   };
 
   const handleClickOnSelectStudio = (studioId: number) => {
@@ -77,7 +109,6 @@ export const ContextProvider: React.FC<IChildren> = ({ children }) => {
 
   const handleClickOnCategory = (categoryId: number | null) => {
     setActiveCategory(categoryId);
-    setShowAllGames(false);
   };
 
   const handleClickOnCurrency = (currency: string) => {
@@ -89,21 +120,30 @@ export const ContextProvider: React.FC<IChildren> = ({ children }) => {
     setStagedSelectedStudios([]);
   };
 
+  const handleClickOnLoadMoreGames = (load: boolean) => {
+    if (load) {
+      setNumberOfGamesVisible(prev => prev + 50);
+    } else {
+      setNumberOfGamesVisible(50);
+    }
+  };
+
   const contextValue = {
     modalStates: modalStates,
     selectedCurrency: selectedCurrency,
     categories: categories,
+    filteredGames: filteredGames,
     stagedSelectedStudios: stagedSelectedStudios,
-    showAllGames: showAllGames,
     activeCategory: activeCategory,
     studios: studios,
+    numberOfGamesVisible: numberOfGamesVisible,
     toggleModalOnClick: toggleModalOnClick,
     handleClickOnCurrency: handleClickOnCurrency,
     handleClickOnCategory: handleClickOnCategory,
     handleClickOnSelectButton: handleClickOnSelectButton,
     handleClickOnSelectStudio: handleClickOnSelectStudio,
     handleClickOnResetStudios: handleClickOnResetStudios,
-    toggleShowAllGamesOnClick: toggleShowAllGamesOnClick,
+    handleClickOnLoadMoreGames: handleClickOnLoadMoreGames,
   };
 
   return <Context.Provider value={contextValue}>{children}</Context.Provider>;
